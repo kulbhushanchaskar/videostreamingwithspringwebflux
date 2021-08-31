360913241d30e2580d5e1e7ad2877b4c82a6f5cf
vn51sci <vn51sci@homeoffice.wal-mart.com>
2021-08-31 15:07:33 +0530

    video streaming tutorial

---
+++ src/main/java/com/videostreaming/VideostreamingApplication.java
@@ -0,0 +1,31 @@
+package com.videostreaming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class VideostreamingApplication {
+	
+	@Autowired
+	private StreamingService s;
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostreamingApplication.class, args);
+	}
+	
+	@GetMapping(value="video/{title}", produces="video/mp4")
+	public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range) {
+		System.out.println("Range="+range);
+		return s.getVideo(title);
+	}
+
+}
